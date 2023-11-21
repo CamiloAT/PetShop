4ba83855b0e307aa144258347431182896b54f94
Juan Jose Albornoz <juan.albornoz@uptc.edu.co>
2023-11-21 15:38:56 -0500

    se arregla metodo del HttpRequest

--- src/main/java/co/edu/uptc/request/HttpDemand.java
+++ src/main/java/co/edu/uptc/request/HttpDemand.java
@@ -15,17 +15,16 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class HttpDemand {
+	HttpURLConnection connection;
+	URL url;
 
-	public HttpURLConnection connect() {
-		URL url;
-		HttpURLConnection connection = null;
+	public void connect() {
 		try {
 			url = new URL("http://localhost:8080/pets/save");
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("POST");
 			connection.setDoOutput(true);
 			connection.setRequestProperty("Content-Type", "application/json");
-			return connection;
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,29 +32,31 @@ public class HttpDemand {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return connection;
 
 	}
 
 	public String requestSave(Pet petToSend) throws Exception {
+		connect();
 		// Convierte el objeto Pet a formato JSON
 		ObjectMapper objectMapper = new ObjectMapper();
+		petToSend.setId((long) 1);
+		petToSend.setName("Darla");
+		petToSend.setCategory("perro");
 		String requestBody = objectMapper.writeValueAsString(petToSend);
 		// Escribe los datos en el cuerpo de la solicitud
-		try (OutputStream os = connect().getOutputStream()) {
+		try (OutputStream os = connection.getOutputStream(); ) {
 			byte[] input = requestBody.getBytes(StandardCharsets.UTF_8);
 			os.write(input, 0, input.length);
 		}
 		// Obtiene el código de respuesta
-		int responseCode = connect().getResponseCode();
+		int responseCode = connection.getResponseCode();
 		System.out.println("Código de respuesta: " + responseCode);
 		String line;
 		// Lee la respuesta del servidor
 		if (responseCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(connect().getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 			StringBuilder response = new StringBuilder();
 			while ((line = reader.readLine()) != null) {
 				response.append(line);
@@ -69,7 +70,8 @@ public class HttpDemand {
 
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		HttpDemand htt= new  HttpDemand();
+		htt.requestSave(new Pet());
 	}
 }
